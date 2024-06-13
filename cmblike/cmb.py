b4b3cee4c3992ed0b2a3f52cc9980f35e90ae26c
htjb <harrytjbevins@yahoo.co.uk>
2024-06-13 17:04:43 +0100

    tidying up the code

--- cmblike/cmb.py
+++ cmblike/cmb.py
@@ -6,7 +6,7 @@ from cmblike.noise import planck_noise, wmap_noise
 import yaml
 
 class CMB():
-    def __init__(self, config, **kwargs):
+    def __init__(self, **kwargs):
 
         """
         Class to generate CMB power spectra from CAMB or cosmopower,
@@ -14,69 +14,74 @@ class CMB():
 
         Parameters:
         -----------
-        parameters: list
-            The cosmological parameters to sample over. Defaults to
-            ['omegabh2', 'omegach2', 'thetaMC', 'tau', 'ns', 'As'].
-        prior_mins: list
-            The minimum values for the priors. Defaults to
-            [0.01, 0.08, 0.97, 0.01, 0.8, 2.6].
-        prior_maxs: list
-            The maximum values for the priors. Defaults to
-            [0.085, 0.21, 1.5, 0.16, 1.2, 3.8].
-        default_parameter_values: list
-            The default values for the cosmological parameters (inspired by
-            Planck observations). Defaults to
-            [0.022, 0.12, 1.04, 0.055, 0.965, 3.0].
+        path_to_cp: str
+            The path to the cosmopower directory. Defaults to None.
         """
         
-        if not type(config) == dict:
-            with open(config, 'r') as f:
-                self.config = yaml.safe_load(f)
+        self.path_to_cp = kwargs.get('path_to_cp', None)
 
-        if config['path_to_cp']:
+        if self.path_to_cp:
             self.cp_nn = cp.cosmopower_NN(restore=True, 
-                                restore_filename= config['path_to_cp'] \
-                                +'/cosmopower/trained_models/CP_paper/CMB/cmb_TT_NN')
+                    restore_filename= self.path_to_cp \
+                    +'/cosmopower/trained_models/CP_paper/CMB/cmb_TT_NN')
         else:
             self.pars = camb.CAMBparams()
     
-    def get_cosmopower_model(self):
+    def get_cosmopower_model(self, parameters):
+
+        """
+        Function to get the cosmopower model for the CMB power spectrum.
+
+        Parameters:
+        -----------
+        theta: dictionary
+            An dictionary of cosmological parameters. For cosmopower the
+            required parameters are 'omegabh2', 'omegach2', 'h', 
+            'tau', 'ns', 'As'.
         
-        params = {'omega_b': [float(self.config['parameters']['omegabh2'])],
-                'omega_cdm': [float(self.config['parameters']['omegach2'])],
-                'h': [float(self.config['parameters']['h'])],
-                'tau_reio': [float(self.config['parameters']['tau'])],
-                'n_s': [float(self.config['parameters']['ns'])],
-                'ln10^{10}A_s': [float(self.config['parameters']['As'])],
+        Returns:
+        --------
+        spectra: array
+            The CMB power spectrum at l=2 to 2508.
+        """
+        
+        params = {'omega_b': [parameters['omegabh2']],
+                'omega_cdm': [parameters['omegach2']],
+                'h': [parameters['h']],
+                'tau_reio': [parameters['tau']],
+                'n_s': [parameters['ns']],
+                'ln10^{10}A_s': [parameters['As']],
                 }
         
         spectra = self.cp_nn.ten_to_predictions_np(params)[0]*1e12*2.725**2
         return spectra
     
-    def get_camb_model(self):
+    def get_camb_model(self, parameters):
 
         """
         Function to get the CAMB model for the CMB power spectrum.
 
         Parameters
         ----------
-        theta: array
-            The cosmological parameters as generated from self.prior.
+        theta: dictionary
+            An dictionary of cosmological parameters. For CAMB the
+            required parameters are 'omegabh2', 'omegach2', 'thetaMC', 
+            'tau', 'ns', 'As'.
         
-        Returns
-        -------
+        Returns:
+        --------
         cl: array
-            The CMB power spectrum.
+            The CMB power spectrum at l=2 to 2508.
         """
 
-        self.pars.set_cosmology(ombh2=float(self.config['parameters']['omegabh2']),
-                omch2=float(self.config['parameters']['omegach2']),
-                tau=float(self.config['parameters']['tau']), 
-                cosmomc_theta=float(self.config['parameters']['thetaMC'])/100,
+        self.pars.set_cosmology(ombh2=parameters['omegabh2'],
+                omch2=parameters['omegach2'],
+                tau=parameters['tau'], 
+                cosmomc_theta=parameters['thetaMC']/100,
                 theta_H0_range=[5, 1000])
         self.pars.InitPower.set_params(
-            As=np.exp(float(self.config['parameters']['As']))/10**10, 
-            ns=float(self.config['parameters']['ns']))
+            As=np.exp(parameters['As'])/10**10, 
+            ns=parameters['ns'])
         self.pars.set_for_lmax(2500, lens_potential_accuracy=0)
 
         # computes evolution of background cosmology
@@ -86,9 +91,11 @@ class CMB():
                                            CMB_unit='muK')['total'][:,0]
         # convert to C_l
         cl *= (2*np.pi)/(np.arange(len(cl))*(np.arange(len(cl))+1))
+
+        cl = np.interp(np.arange(2, 2509), np.arange(0, len(cl)), cl)
         return cl
     
-    def get_samples(self, l, theta, bins, noise=None, cp=None):
+    def get_samples(self, theta, bins, noise_profile):
 
         """
         Code to generate observations of a theoretical CMB power spectrum.
